509478a8e473bffee1a5393010fed9352325d7e1
Hasan Uzun <hasnuzn@gmail.com>
2025-05-22 10:27:10 +0300

    Update

--- extension.js
+++ extension.js
@@ -1,33 +1,22 @@
 const vscode = require('vscode');
 
 function activate(context) {
-  const disposable = vscode.commands.registerCommand('extension.moveToQuickOpen', () => {
-    const editor = vscode.window.activeTextEditor;
-    if (!editor) {
-      return;
+  const disposable = vscode.commands.registerCommand(
+    'extension.moveToQuickOpen',
+    () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) { return; }
+
+      // Seçili metni al ve \\ → \ dönüştür
+      const text = editor.document.getText(editor.selection).replace(/\\\\/g, '\\');
+
+      // Quick Open’i önceden doldurulmuş metinle aç
+      vscode.commands.executeCommand('workbench.action.quickOpen', text);
     }
-
-    const selection = editor.document.getText(editor.selection);
-
-    // Kopyalanan metindeki \\ karakterlerini \ ile değiştir
-    const modifiedSelection = selection.replace(/\\\\/g, '\\');
-
-    // Mevcut clipboard içeriğini alıp, sonrasında geri yüklemek için bir promise zinciri oluşturuyoruz
-    vscode.env.clipboard.readText().then(oldText => {
-      // Quick Open penceresine metni doğrudan gönder
-      vscode.env.clipboard.writeText(modifiedSelection);
-      vscode.commands.executeCommand("workbench.action.quickOpen");
-      vscode.commands.executeCommand("editor.action.clipboardPasteAction");
-      vscode.env.clipboard.writeText(oldText);
-    });
-  });
+  );
 
   context.subscriptions.push(disposable);
 }
 
-function deactivate() {}
-
-module.exports = {
-  activate,
-  deactivate
-};
+exports.activate   = activate;
+exports.deactivate = () => {};
